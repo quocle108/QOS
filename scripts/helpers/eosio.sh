146a65dae3502ca281d3bd54568c984cbce329ab
Quoc Le <songmai108@gmail.com>
2020-10-13 11:28:54 +0700

    QOS migration

--- scripts/helpers/eosio.sh
+++ scripts/helpers/eosio.sh
@@ -118,10 +118,7 @@ function previous-install-prompt() {
 }
 
 function resources() {
-    echo "${COLOR_CYAN}EOSIO website:${COLOR_NC} https://eos.io"
-    echo "${COLOR_CYAN}EOSIO Telegram channel:${COLOR_NC} https://t.me/EOSProject"
-    echo "${COLOR_CYAN}EOSIO resources:${COLOR_NC} https://eos.io/resources/"
-    echo "${COLOR_CYAN}EOSIO Stack Exchange:${COLOR_NC} https://eosio.stackexchange.com"
+    echo "${COLOR_CYAN}QOS resources:${COLOR_NC} https://github.com/quocle108/QOS"
 }
 
 function print_supported_linux_distros_and_exit() {
